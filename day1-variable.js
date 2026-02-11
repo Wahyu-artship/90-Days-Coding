1908349e5433ee2992172861ea7fabafcd4171fc
Wahyu-artship <ryzalwhyy@gmail.com>
2026-02-11 17:47:49 +0700

    Day1 javascript Variable - First Code!

---
+++ day1-variable.js
@@ -0,0 +1,29 @@
+// Day 1 : Javascript Variable
+// Time : [11-02-2026]
+
+console.log("=== Day 1 :JAVASCRIPT VARIABLES ===");
+
+// Task 1 : Token Information
+const tokenName = "Wahyu Token";
+const tokenSymbol = "WHY";
+let totalSupply = "1000000"; // 1 million tokens
+
+console.log("Token Name:", tokenName);
+console.log("Token Symbol:", tokenSymbol);
+console.log("Total Supply;", totalSupply);
+
+// Task 2 : Burn Tokens
+totalSupply = totalSupply - 1000; // Burn 1000 tokens
+console.log("After burn:", totalSupply);
+
+// Task 3 : My Info
+const myName = "Wahyu";
+const myAge = 21;
+const isLearning = true;
+
+console.log("\n=== My Info ===");
+console.log("Name;", myName);
+console.log("Age;", myAge);
+console.log("Is Learning Javascript?", isLearning);
+
+console.log("\n✅ Day 1 Complete!");
